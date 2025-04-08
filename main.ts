1510a33971bfa011995ff34004cf0317f8231375
newseru <santimir@protonmail.com>
2025-04-08 13:29:17 +0100

    chore: move style to html.

--- main.ts
+++ main.ts
@@ -1,42 +1,44 @@
+// we need a canvas to write out results.
 const canvas = document.querySelector("canvas");
-const body = document.querySelector("body");
-if (body) {
-  body.style.backgroundColor = "yellow";
+if (!canvas) {
+  throw new Error("No Canvas HTML Element found in page.");
 }
+
 if (!navigator.gpu) {
-  // browser support
   throw new Error("WebGPU not supported on this browser.");
 }
+
+// Get GPU
 const adapter = await navigator.gpu.requestAdapter();
 if (!adapter) {
-  // hardware support
   throw new Error("No appropriate GPUAdapter found.");
 }
-// get an actual gpu
+// get logical device
 const device = await adapter.requestDevice();
+device.lost.then((info) => console.error("Device Lost: ", info));
 
-// link device and memory with canvas
-if (!canvas) {
-  throw new Error("Canvas undefined");
-}
+// link device and canvas
 const context = canvas.getContext("webgpu");
 const texture = navigator.gpu.getPreferredCanvasFormat();
 if (!context) {
-  throw new Error("Context undefined.");
+  throw new Error("Context is `null`.");
 }
 context.configure({
   device: device,
   format: texture,
 });
+
 const encoder = device.createCommandEncoder();
 
+// there are compute and render passes.
+// but we want to render.
 const cPass = encoder.beginRenderPass({
   colorAttachments: [
     {
       view: context.getCurrentTexture().createView(),
       loadOp: "clear",
       storeOp: "store",
-      clearValue: [0.1, 0, 0.7, 0.1], // alpha doesn't work
+      clearValue: [0.1, 0, 0.7, 0.1], // alpha doesn't work?
     },
   ],
 });
