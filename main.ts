5a38f1c7e1d37d59142963da69510434249abb23
newseru <santimir@protonmail.com>
2025-04-11 10:07:14 +0100

    chore: add group(0) comment.

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { randomBetween } from "./randomBetween";
 
-const NUM_BALLS = 10;
-const NUM_BALL_ELEMENTS = 6 * NUM_BALLS;
-const BUFFER_SIZE = NUM_BALL_ELEMENTS * Float32Array.BYTES_PER_ELEMENT;
+const NUM_BALLS = 120; // 120
+const NUM_BALL_ELEMENTS = 6 * NUM_BALLS; // 720
+const BUFFER_SIZE = NUM_BALL_ELEMENTS * Float32Array.BYTES_PER_ELEMENT; //2880
 
 /** Set-Up Section: Canvas and Checks. */
 const canvas = document.querySelector("canvas");
@@ -13,7 +13,6 @@ if (!(canvas instanceof HTMLCanvasElement)) {
 if (!navigator.gpu) {
   throw new Error("Browser does not support WebGPU.");
 }
-
 const adapter = await navigator.gpu.requestAdapter();
 if (!adapter) {
   throw new Error("The GPU isn't being detected, or it is not supported.");
@@ -40,32 +39,28 @@ const module = device.createShaderModule({
         position:vec2f,
         velocity:vec2f,
       }
-      /* Below, two Binds are declared, and need be
-         created outside the shader.
-
-         'input': is the buffer we wrote-in with JS data. 
-         It's a read-only GPUBuffer.
-
-         'output': is the internal buffer that it writes to.
-         it needs read to copy it to a staging buffer.
-
+      /*
+         'io_buffer': is a read / write buffer, copies to a staging buffer.
          We define a @compute shader.
-
          'workgroup_size': number of work items.
          any skipped param is 1. So (64)=(64,1,1)
-      */
-      @group(0) @binding(0) var<storage,read> input: array<Ball>;
-      @group(0) @binding(1) var<storage,read_write> output: array<Ball>;
+       */
+      @group(0) @binding(0) var<storage,read_write> io_buffer: array<Ball>;
       @compute @workgroup_size(64)
       fn main(@builtin(global_invocation_id) global_id: vec3u, @builtin(local_invocation_id) local_id: vec3u) {
+        if(global_id.x >= arrayLength(&io_buffer)){
+          // test if this happen writing something
+          io_buffer[0].radius = f32(global_id.x);
+          return;
+        }
         const TIME_STEP:f32 = 1/60.;
-        var o:Ball = output[global_id.x]; 
-        o.radius = 999.;
-        o.velocity=vec2f(local_id.xy);
-        o.position =
-        input[global_id.x].position +
-        input[global_id.x].velocity * TIME_STEP;
-        output[global_id.x] = o;
+        // we can't just mutate ball, we need to write back.
+        // 1. extract item (ball) in variable
+        var ball = io_buffer[global_id.x];
+        // 2. edit it
+        ball.position = ball.position + ball.velocity * TIME_STEP;
+        // 3. write it back
+        io_buffer[global_id.x] = ball;
   }
     `,
 });
@@ -79,14 +74,8 @@ const bindGroupLayout = device.createBindGroupLayout({
       binding: 0,
       visibility: GPUShaderStage.COMPUTE,
       buffer: {
-        type: "read-only-storage",
+        type: "storage",
       },
-    },
-    {
-      // output is read-write storage.
-      binding: 1,
-      visibility: GPUShaderStage.COMPUTE,
-      buffer: { type: "storage" },
     },
   ],
 });
@@ -94,7 +83,7 @@ const bindGroupLayout = device.createBindGroupLayout({
 const pipeline = device.createComputePipeline({
   label: "Compute Pipeline",
   layout: device.createPipelineLayout({
-    bindGroupLayouts: [bindGroupLayout],
+    bindGroupLayouts: [bindGroupLayout], // index(0) must match group(0)
   }),
   compute: {
     module,
@@ -112,26 +101,20 @@ for (let i = 0; i < inputBalls.length; i += 6) {
   inputBalls[i + 4] = randomBetween(-100, 100); // velocity.x
   inputBalls[i + 5] = randomBetween(-100, 100); // velocity.y
 }
-
-const inputBuffer = device.createBuffer({
+const IOBuffer = device.createBuffer({
   size: BUFFER_SIZE,
-  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-  label: "Input Buffer",
+  usage:
+    GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
+  label: "Input/Output GPUBuffer",
 });
 // Allows to skip staging buffer for the input.
-device.queue.writeBuffer(inputBuffer, 0, inputBalls.buffer);
+device.queue.writeBuffer(IOBuffer, 0, inputBalls);
 
-// This one is for the output.
+// We copy here from the Internal
 const stagingBuffer = device.createBuffer({
   size: BUFFER_SIZE,
   usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
   label: "Staging Buffer",
-});
-
-const outputBuffer = device.createBuffer({
-  label: "Internal Buffer",
-  size: BUFFER_SIZE,
-  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
 });
 
 const computeBindGroup = device.createBindGroup({
@@ -140,50 +123,49 @@ const computeBindGroup = device.createBindGroup({
   entries: [
     {
       binding: 0,
-      resource: { buffer: inputBuffer },
-    },
-    {
-      binding: 1,
-      resource: { buffer: outputBuffer },
+      resource: { buffer: IOBuffer },
     },
   ],
 });
 
-/** Encode the commands */
-const encoder = device.createCommandEncoder({
-  label: "Command Encoder",
-});
-const computePass = encoder.beginComputePass({
-  label: "Compute Pass",
-});
-computePass.setPipeline(pipeline); // Pipeline is conceptual, takes the bind-group layout and the compute shader
-computePass.setBindGroup(0, computeBindGroup); // group 0
-computePass.dispatchWorkgroups(Math.ceil(BUFFER_SIZE / (4 * 64)));
-computePass.end();
+async function run() {
+  /** Encode the commands */
+  const encoder = device.createCommandEncoder({
+    label: "Command Encoder",
+  });
+  const computePass = encoder.beginComputePass({
+    label: "Compute Pass",
+  });
+  computePass.setPipeline(pipeline); // Pipeline is conceptual, takes the bind-group layout and the compute shader
+  computePass.setBindGroup(0, computeBindGroup); // group 0
+  computePass.dispatchWorkgroups(Math.ceil(NUM_BALLS / 64));
+  computePass.end();
 
-// copies data to staging buffer!
-encoder.copyBufferToBuffer;
-encoder.copyBufferToBuffer(
-  outputBuffer, // the place shader writes to
-  0,
-  stagingBuffer, // intermediary
-  0,
-  BUFFER_SIZE
-);
-const commands = encoder.finish();
-device.queue.submit([commands]);
+  // copies data to staging buffer!
+  encoder.copyBufferToBuffer(
+    IOBuffer, // the place shader writes to
+    0,
+    stagingBuffer, // intermediary
+    0,
+    BUFFER_SIZE
+  );
+  const commands = encoder.finish();
+  device.queue.submit([commands]);
 
-// make staging buffer data available to JS
-await stagingBuffer.mapAsync(GPUMapMode.READ, 0, BUFFER_SIZE);
-if (stagingBuffer.mapState === "mapped") {
-  const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
-  // copy it
-  const data = copyArrayBuffer.slice();
-  // release the GPU memory.
-  stagingBuffer.unmap(); // AB is detached.
-  console.log(new Float32Array(data));
-} else {
-  console.error("mapAsync did not map the staging buffer.");
+  // make staging buffer data available to JS
+  await stagingBuffer.mapAsync(GPUMapMode.READ, 0, BUFFER_SIZE);
+  if (stagingBuffer.mapState === "mapped") {
+    const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
+    // copy it
+    const data = copyArrayBuffer.slice();
+    // release the GPU memory.
+    stagingBuffer.unmap(); // AB is detached.
+    console.log(new Float32Array(data));
+  } else {
+    console.error("mapAsync did not map the staging buffer.");
+  }
 }
-
+await run();
+await run();
 export {};
+
